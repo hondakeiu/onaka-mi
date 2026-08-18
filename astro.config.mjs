569cb6c110429900f3e388914e452adbd5bda277
honda keiu <k-honda@studio-spoon.co.jp>
2026-08-19 02:56:50 +0900

    additionalData

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hondakeiu.github.io",
-  base: "onaka-mi",
-  output: "static",
-  // vite: {
-  //   css: {
-  //     preprocessorOptions: {
-  //       scss: {
-  //         additionalData: `
-  //           @use "/src/styles/mixins/mq.scss" as *;
-  //         `,
-  //       },
-  //     },
-  //   },
-  // },
+  site: 'https://hondakeiu.github.io',
+  base: 'onaka-mi',
+  output: 'static',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "/src/styles/mixins/mq.scss" as *;
+          `,
+        },
+      },
+    },
+  },
 });
